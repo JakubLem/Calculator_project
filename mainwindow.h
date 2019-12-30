1ca2c366eae6abfcdce58e0dc4c001b6bf69fc20
JakubLem <49617551+JakubLem@users.noreply.github.com>
2019-12-30 15:56:44 +0100

    Initial commit

---
+++ mainwindow.h
@@ -0,0 +1,36 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QDebug>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+    double firstnumber = NULL, secondnumber = NULL, result = NULL;
+    bool checker = false;
+    int type = NULL;
+private:
+    Ui::MainWindow *ui;
+
+private slots:
+    void digital_pressed();
+
+    void on_pushButton_dot_released();
+
+
+    bool isdothere(QString a);
+    void on_pushButton_plusminus_released();
+    void on_pushButton_C_clicked();
+    void operation_pressed();
+    void on_pushButton_equal_clicked();
+};
+#endif // MAINWINDOW_H
